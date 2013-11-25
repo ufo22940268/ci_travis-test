4a40dcf93fa42c5010c572fb5e2667051bc20315
garlic <ufo22940268@gmail.com>
2013-11-25 10:46:48 +0800

    up

---
+++ test_main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2013 garlic <garlic@localhost>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+def test_main():
+    assert 1 == 2
